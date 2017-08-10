28bdb05ab53edc67ac492359242cd9946617e687
HayelomR <rhayelom@gmail.com>
2017-08-10 17:52:26 -0600

    change on the app

---
+++ app/data/friends.js
@@ -0,0 +1,68 @@
+var friends = [
+{
+  "name":"Taylor Swift",
+  "photo":'<a href="http://tinypic.com?ref=2uygn54" target="_blank"><img src="http://i43.tinypic.com/2uygn54.jpg" border="0" alt="Image and video hosting by TinyPic"></a>',
+  "scores":[
+      5,
+      1,
+      4,
+      4,
+      1,
+      5,
+      5,
+      2,
+      3,
+      5
+    ]
+  },
+{
+  "name":"Justin Bieber",
+  "photo":'<a href="http://tinypic.com?ref=2uqlz4p" target="_blank"><img src="http://i58.tinypic.com/2uqlz4p.jpg" border="0" alt="Image and video hosting by TinyPic"></a>',
+  "scores":[
+      1,
+      2,
+      3,
+      5,
+      4,
+      1,
+      5,
+      5,
+      3,
+      5
+    ]
+},{
+  "name":"Katy Perry",
+  "photo":'<a href="http://tinypic.com?ref=2vmg3er" target="_blank"><img src="http://i60.tinypic.com/2vmg3er.jpg" border="0" alt="Image and video hosting by TinyPic"></a>',
+  "scores":[
+      4,
+      1,
+      1,
+      4,
+      3,
+      1,
+      2,
+      4,
+      5,
+      5
+    ]
+},
+{
+  "name":"Liya Kebede",
+  "photo":'<a href="http://tinypic.com?ref=5dtm6u" target="_blank"><img src="http://i39.tinypic.com/5dtm6u.jpg" border="0" alt="Image and video hosting by TinyPic"></a>',
+  "scores":[
+      4,
+      1,
+      4,
+      2,
+      1,
+      1,
+      1,
+      2,
+      3,
+      1
+    ]
+}
+
+];
+
+module.exports = friends;
--- server.js
+++ server.js
@@ -1,17 +1,21 @@
 var express = require("express");
-var bodyParser = require("body-parser");
 var app = express();
-var PORT = process.env.PORT || 8080;
+var bodyParser = require("body-parser");
+var PORT = process.env.PORT || 3000;
+var jsonParser = bodyParser.json();
+// create application/x-www-form-urlencoded parser 
+var urlencodedParser = bodyParser.urlencoded({ extended: false })
 
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(bodyParser.text());
-app.use(bodyParser.json({ type: "application/vnd.api+json" }));
-
-// require("./app/routing/apiRoutes")(app);
-// require("./app/routing/htmlRoutes")(app);
-
+// parse various different custom JSON types as JSON 
+app.use(bodyParser.json({ type: 'application/*+json' }))
+// parse some custom thing into a Buffer 
+app.use(bodyParser.raw({ type: 'application/vnd.custom-type' }))
+// parse an HTML body into a string 
+app.use(bodyParser.text({ type: 'text/html' }))
+//require("./app/routing/apiRoutes.js")(app);
+require("./app/routing/apiRoutes.js")(app);
+require("./app/routing/htmlRoutes.js")(app);
 
 app.listen(PORT, function() {
-  console.log("App listening on PORT: " + PORT);
+	console.log("App listening on PORT: " + PORT);
 });
