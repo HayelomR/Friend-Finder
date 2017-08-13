2ca6764ce2dcc6048a6f30728e62ac1ffce3bb99
HayelomR <rhayelom@gmail.com>
2017-08-12 21:08:49 -0600

    final commit

--- app/data/friends.js
+++ app/data/friends.js
@@ -41,18 +41,37 @@ var friends = [
     "name": "Rihanna",
     "photo": "http://assets4.capitalxtra.com/2017/07/rihanna-grammys-2017-1486951728-editorial-short-form-0.jpg",
     "scores":[
-        1,
-        1,
-        1,
-        1,
-        1,
-        1,
-        1,
-        1,
-        1,
-        1
+        5,
+        4,
+        3,
+        5,
+        5,
+        5,
+        4,
+        5,
+        4,
+        3
+    ]
+  },
+  {
+    "name": "Britney Spears",
+    "photo": "http://www.billboard.com/files/media/Britney-Spears-mtv-vmas-04-billboard-white-carpet-2016-bb-1548.jpg",
+    "scores":[
+        5,
+        5,
+        4,
+        3,
+        2,
+        5,
+        4,
+        5,
+        5,
+        4
     ]
   }
+
+
+
 ];
 
 // Note how we export the array. This makes it accessible to other files using require.
